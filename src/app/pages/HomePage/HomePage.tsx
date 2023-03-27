fdbdb9eb0af577d2addeabd0bfc89e8954a19a5d
duythanh <ints.thanhdd>
2023-03-27 17:19:04 +0700

    update UI

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -7,31 +7,32 @@ const HomePage = (props: Props) => {
     <div className={s.wrapper}>
       <div className={s.sideChat}>
         <ul className={s.listChat}>
-          <li className={s.chatItem}>
-            <img
-              src="https://udayananetworking.unud.ac.id/assets/frontend/images/user-m.png"
-              alt=""
-              className={s.avtUser}
-            />
-            <div className={s.infoUser}>
-              <p className={s.name}>chat</p>
-              <p className={s.message}>
-                Ban : xin chao moi nguoi toi la tizz dep try skd sieu dinh
-              </p>
-            </div>
-          </li>
-          <li className={s.chatItem}>
-            <p>chat</p>
-          </li>
-          <li className={s.chatItem}>
-            <p>chat</p>
-          </li>
-          <li className={s.chatItem}>
-            <p>chat</p>
-          </li>
+          {Array(20)
+            .fill(1)
+            .map(() => (
+              <li className={s.chatItem}>
+                <img
+                  src="https://udayananetworking.unud.ac.id/assets/frontend/images/user-m.png"
+                  alt=""
+                  className={s.avtUser}
+                />
+                <div className={s.infoUser}>
+                  <p className={s.name}>chat</p>
+                  <p className={s.message}>
+                    Ban : xin chao moi nguoi toi la tizz dep try skd sieu dinh
+                  </p>
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
-      <div className={s.contentChat}></div>
+      <div className={s.contentChat}>
+        <div className={s.messageList}></div>
+        <div className={s.inputWrap}>
+          <input type="text" className={s.inputMess} />
+          <span className={s.btnSend}>Send</span>
+        </div>
+      </div>
     </div>
   );
 };
