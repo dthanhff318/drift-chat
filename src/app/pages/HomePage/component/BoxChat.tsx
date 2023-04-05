d5da67a9994e0505b04916fde7acf2ebab98a5f7
duythanh <ints.thanhdd>
2023-04-05 13:41:15 +0700

    update ui chatting

--- src/app/pages/HomePage/component/BoxChat.tsx
+++ src/app/pages/HomePage/component/BoxChat.tsx
@@ -1,6 +1,8 @@
+import Icon, { SendOutlined } from "@ant-design/icons";
 import Avatar from "app/components/Avatar/Avatar";
 import React from "react";
 import s from "./style.module.scss";
+import c from "clsx";
 type Props = {};
 
 const BoxChat = (props: Props) => {
@@ -14,12 +16,24 @@ const BoxChat = (props: Props) => {
         {Array(20)
           .fill(1)
           .map((e, i) => (
-            <div className={s.message}>heloo</div>
+            <div className={c(s.message, i % 2 === 0 ? s.left : s.right)}>
+              <div className={s.contentWrap}>
+                <span className={c(s.contentMsg)}>
+                  helooel ooelooel ooeloo ooeloo elooeloo oelooelooelo
+                  oelooelooeloo looelo oelooelooe looelooeloo elooelo looe
+                  looelolooelolooelolooelolooelolooelolooelolooelo
+                </span>
+                <div className={s.options}></div>
+              </div>
+              <p className={s.timeSend}>12:10 PM</p>
+            </div>
           ))}
       </div>
       <div className={s.chatting}>
-        <input className={s.inputChat} type="text" />
-        <button>Send</button>
+        <textarea className={s.inputChat} />
+        <button className={s.sendMsg}>
+          <SendOutlined className={s.iconSend} />
+        </button>
       </div>
     </div>
   );
