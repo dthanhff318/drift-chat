aefe5e69426e41361257a08d80d9c8b8d26d85df
linhnt <leeminho03072000@gmail.com>
2024-01-09 00:36:41 +0700

    logic change nickname

--- src/app/pages/HomePage/component/SideChat/component/ListMember/ListMember.tsx
+++ src/app/pages/HomePage/component/SideChat/component/ListMember/ListMember.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import s from "./style.module.scss";
 import { TGroup } from "types/common";
 import {
@@ -13,19 +13,36 @@ type Props = {
 const ListMember = ({ detailGroup }: Props) => {
   const { members } = detailGroup;
   const [edit, setEdit] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEditNickname = async () => {
+    const newNickname = inputRef.current?.value;
+    console.log(newNickname);
+  };
+
   return (
     <div className={s.wrapper}>
       {members?.map((e) => (
-        <div className={s.member}>
+        <div className={s.member} key={e.id}>
           <div className={s.nameWrap}>
             <div className={s.nicknameWrap}>
               {edit === e.id ? (
-                <input type="text" className={s.inputEditNickname} />
+                <input
+                  type="text"
+                  ref={inputRef}
+                  className={s.inputEditNickname}
+                />
               ) : (
                 <span className={s.nickname}>tiz dep try</span>
               )}
               {edit === e.id ? (
-                <div className={s.icChangeNickname} onClick={() => setEdit("")}>
+                <div
+                  className={s.icChangeNickname}
+                  onClick={() => {
+                    handleEditNickname();
+                    setEdit("");
+                  }}
+                >
                   <CheckCircleOutlined />
                 </div>
               ) : (
