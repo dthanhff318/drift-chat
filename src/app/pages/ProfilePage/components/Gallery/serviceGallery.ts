926a81f858ed30d2fa042d1e5511f9ffc61dfcb4
linhnt <leeminho03072000@gmail.com>
2024-03-13 00:09:09 +0700

    Fix logic get post

--- src/app/pages/ProfilePage/components/Gallery/serviceGallery.ts
+++ src/app/pages/ProfilePage/components/Gallery/serviceGallery.ts
@@ -1,6 +1,9 @@
+import postApi from 'app/axios/api/postApi';
 import postStore from 'app/storeZustand/postStore';
 import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { TPost } from 'types/post.type';
 
 type TModalGallery = '' | 'create' | 'detail';
 
@@ -8,8 +11,14 @@ export const useServiceGallery = () => {
   const [modal, setModal] = useState<TModalGallery>('');
   const { userId } = useParams<{ userId: string }>();
   const { getPosts, savePostDetail, loadingPost, posts } = postStore();
+
+  const { data, isLoading } = useQuery<{ data: TPost[] }>({
+    queryKey: ['get', 'post'],
+    queryFn: () => postApi.getPosts(),
+  });
+
   useEffect(() => {
     getPosts(userId);
   }, [userId]);
-  return { modal, loadingPost, posts, userId, setModal, savePostDetail };
+  return { data, isLoading, modal, loadingPost, posts, userId, setModal, savePostDetail };
 };
